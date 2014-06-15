58214d5b51d532db6d95ec4b55ccc10eb360576e
karinmae <kmaennersdorfer@gmx.at>
2014-06-15 23:36:36 +0200

    login

--- php/db/login.php
+++ php/db/login.php
@@ -1,8 +1,16 @@
 <?php
 
 session_start();
+$_SESSION["status"] = array();
+
+if (!isset($_POST["uid"]) && !isset($_POST["pw"])) {
+	$_SESSION["status"] = 0;
+	header('Location:../start.php');
+}
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['uid']) && isset($_POST['pw']))
 {
+        $_SESSION["status"] = 2;
+        $_SESSION["loggedin"] = true;
 	$user = $_POST['uid'];
 	$pass = $_POST['pw'];
 	
@@ -12,8 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['uid']) && isset($_POST
 	$db->Connect();
 	$result = $db->Query("SELECT * FROM `user` WHERE `Username`=? AND `Password`=?;", 'ss', htmlentities($user, (ENT_QUOTES | ENT_XHTML)), md5($pass));
 	$db->Close();
+        header('Location:../index.php');
 }
-header('Location:../index.php');
 
+
+else {
+	$_SESSION["status"] = 0;
+	header('Location:../start.php'); 
+	exit(); 
+}
 
 
--- php/start.php
+++ php/start.php
@@ -10,7 +10,7 @@
             <img id="logo" src="../Logo.png" width="350" height="100"/>
         <div id="login">
             <form action="db/login.php" method="post" id="login">
-                    <input type="text" name="uid" placeholder="E-Mail">
+                    <input type="text" name="uid" placeholder="E-Mail" style="width:170px;">
                     <input type="password" name="pw" placeholder="Passwort">
                     <input type="submit" value="Login" />
                 </div>
@@ -20,11 +20,11 @@
         <div id="register">
             <h1>Registrieren</h1>
             <form action="db/newUser.php" method="post" id="newUser">
-                <input type="text" name="firstname" placeholder="Vorname">
-                <input type ="text" name="lastname" placeholder="Nachname">
-                <input type="text" name="mail" placeholder="E-Mail">
-                <input type="password" name="pw1" placeholder="Passwort">
-                <input type="password" name="pw2" placeholder="Passwort wiederholen">
+                <input type="text" name="firstname" placeholder="Vorname" style="width:150px;">
+                <input type ="text" name="lastname" placeholder="Nachname" style="width:150px;">
+                <input type="text" name="mail" placeholder="E-Mail" style="width:338px;">
+                <input type="password" name="pw1" placeholder="Passwort" style="width:338px;">
+                <input type="password" name="pw2" placeholder="Passwort wiederholen" style="width:338px;">
                 <input type="submit" value="Registrieren" />
             </form>
         </div>
