422655a3aa759f80cacf1032aed15e5197465606
karinmae <kmaennersdorfer@gmx.at>
2014-06-15 17:19:51 +0200

    db file

--- db.sql
+++ db.sql
@@ -11,6 +11,8 @@
 
 CREATE TABLE IF NOT EXISTS `user` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Firstname` varchar(50) NOT NULL,
+  `Lastname` varchar (100) NOT NULL,
   `Username` varchar(32) NOT NULL,
   `Password` varchar(128) NOT NULL,
   PRIMARY KEY (`ID`)
