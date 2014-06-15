b5165236a6cb846446fcaf546de8991e7ed704e9
Angelika Reithmayer <if12b061@technikum-wien.at>
2014-06-15 14:37:40 +0200

    documents link hinzugefügt

--- php/index.php
+++ php/index.php
@@ -6,49 +6,49 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        
+
         <link rel="stylesheet" type="text/css" href="menu.css">
         <link rel="stylesheet" type="text/css" href="board.css">
-        
+
         <meta charset="UTF-8">
         <title>KANsimpleBAN-The simple way to manage projects</title>
     </head>
     <body>
         <div id="header">
-        <!-- Das Logo sowie Profilbild in Header -->
-        <img id="logo" src="Logo.png" width="350" height="100"/>
+            <!-- Das Logo sowie Profilbild in Header -->
+            <img id="logo" src="Logo.png" width="350" height="100"/>
         </div>
         <!-- Hier das Akkordeon Menü -->
         <nav>
-    <ul>
-        <li id = "home">
-            <a href="#home">Home</a>
-        </li>
-        <li id = "kalender">
-            <a href="#kalender">Kalender</a>
-        </li>
-        <li id = "projektverwaltung">
-            <a href="#projektverwaltung">Projektverwaltung</a>
             <ul>
-                <li> 
+                <li id = "home">
+                    <a href="#home">Home</a>
+                </li>
+                <li id = "kalender">
+                    <a href="#kalender">Kalender</a>
+                </li>
+                <li id = "projektverwaltung">
+                    <a href="#projektverwaltung">Projektverwaltung</a>
+                    <ul>
+                        <li> 
+                        </li>
+                    </ul>
+                </li>
+                <li id = "stories">
+                    <a href="#stories">Stories</a>
+                </li>
+                <li id = "dokumente">
+                    <a href="documents.php">Dokumente</a>
                 </li>
             </ul>
-        </li>
-                <li id = "stories">
-            <a href="#stories">Stories</a>
-        </li>
-                <li id = "dokumente">
-            <a href="#dokumente">Dokumente</a>
-        </li>
-    </ul>
-</nav>
-     <!-- Hier das Board -->
-     
-     <canvas id="board"
+        </nav>
+        <!-- Hier das Board -->
+
+        <canvas id="board"
     </canvas>
-     </div>        <?php
-       // class_implements($kanBoard, $autoload);
-        //Wollte alles dann in Klassen packen, aber is ja jetzt erstmal wurscht^^
-        ?>
-    </body>
+</div>        <?php
+// class_implements($kanBoard, $autoload);
+//Wollte alles dann in Klassen packen, aber is ja jetzt erstmal wurscht^^
+?>
+</body>
 </html>
